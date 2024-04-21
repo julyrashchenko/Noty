170b632863ce0e02f8bee4f7f055b247b42fe7e0
Iuliia Rashchenko <july.rashchenko@gmail.com>
2024-04-21 21:00:27 +0300

    fix pep issues

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 
 from telegram_bot.telegram_bot import TelegramBot
 
-
 load_dotenv()
 bot = TelegramBot(token=os.environ['TELEGRAM_BOT_ACCESS_TOKEN'])
 print()
-
--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -4,4 +4,3 @@ from telegram.ext import ApplicationBuilder
 class TelegramBot:
     def __init__(self, token: str):
         self.application = ApplicationBuilder().token(token).build()
-
