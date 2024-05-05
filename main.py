8f13765f28aab8ae287fd1e7490b34dbc64cb463
Iuliia Rashchenko <july.rashchenko@gmail.com>
2024-05-05 14:34:29 +0300

    add command handler, finalize telegram bot and run it for the 1st time

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 from telegram_bot.telegram_bot import TelegramBot
 
 load_dotenv()
-bot = TelegramBot(token=os.environ['TELEGRAM_BOT_ACCESS_TOKEN'])
+TelegramBot(token=os.environ['TELEGRAM_BOT_ACCESS_TOKEN']).run_polling()
 print()
--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,6 +1,27 @@
-from telegram.ext import ApplicationBuilder
+from enum import Enum
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+
+
+class Command(Enum):
+    START = 'start'
+
+
+class CommandHandlersFactory:
+    # todo
+    pass
 
 
 class TelegramBot:
     def __init__(self, token: str):
         self.application = ApplicationBuilder().token(token).build()
+        self.application.add_handler(CommandHandler(Command.START.value, self.command_handler))
+
+    def run_polling(self):
+        print('Bot started.')
+        self.application.run_polling()
+
+    async def command_handler(self, update: Update, context: CallbackContext):
+        if update.message.text == f'/{Command.START.value}':
+            await update.message.reply_text('Hi! Hello, world :)')
