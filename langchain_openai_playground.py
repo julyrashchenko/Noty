642f2243034344c6306771ed0f46f6f6ae57d528
Iuliia Rashchenko <july.rashchenko@gmail.com>
2024-05-26 13:55:57 +0300

    use memory storage in playground

--- langchain_openai_playground.py
+++ langchain_openai_playground.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
 from dotenv import load_dotenv
-from langchain_chroma import Chroma
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain_community import document_loaders
+
+from smart_notes.memory_storage import MemoryStorage
 
 load_dotenv()
 
 # todo use the newest model
 llm = ChatOpenAI(model='gpt-3.5-turbo-0125')
 
-# todo perhaps, we don't need a loader: just create a Document
 data_path = Path(__file__).parent / 'data'
 file_names = [
     'test_doc1.txt',
@@ -22,15 +22,18 @@ for file_name in file_names:
     loads = document_loaders.TextLoader(data_path / file_name).load()
     documents.extend(loads)
 
-# todo use a proper embedding (multilingual)
-vectorstore = Chroma.from_documents(documents, embedding=OpenAIEmbeddings())
+memory_storage = MemoryStorage()
+# for document in documents:
+#     memory_storage.store(document.page_content)
 
-retriever = vectorstore.as_retriever(search_type='similarity')
-retrieved_docs = retriever.invoke('What is my place of origin?')
-print(retrieved_docs[0].page_content)
+retrieved_content = memory_storage.withdraw('What is my place of origin?')
+print(retrieved_content)
 
-retrieved_docs = retriever.invoke('Who am I?')
-print(retrieved_docs[0].page_content)
+retrieved_content = memory_storage.withdraw('Who am I?')
+print(retrieved_content)
 
-retrieved_docs = retriever.invoke('What age am I?')
-print(retrieved_docs[0].page_content)
+retrieved_content = memory_storage.withdraw('What age am I?')
+print(retrieved_content)
+
+retrieved_content = memory_storage.withdraw('What do I like?')
+print(retrieved_content)
